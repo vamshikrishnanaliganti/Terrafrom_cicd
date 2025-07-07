e62cf4868b2dbf947f054dbf6fe6de3103b9cde9
vamshikrishnanaliganti <vamshikrishnanaliganti@gmail.com>
2025-07-08 00:59:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,8 @@ provider "aws" {
 resource "aws_instance" "dev" {
     ami = "ami-068990796e29243d5"
     instance_type = "t2.nano"
+    key_name      = "jenkins"
+
     tags = {
       Name = "devvr"
     }

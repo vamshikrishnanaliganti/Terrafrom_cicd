806354d6e2e70a46720c312c50602591619475f0
vamshikrishnanaliganti <vamshikrishnanaliganti@gmail.com>
2025-07-08 01:13:59 +0530

    nad

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-  region = "sa-east-1"
+  region = "ap-south-1"
 }
 
 resource "aws_instance" "dev" {
-    ami = "ami-068990796e29243d5"
+    ami = "ami-0d03cb826412c6b0f"
     instance_type = "t2.nano"
-    key_name      = "jenkins"
+    # key_name      = ""
 
     tags = {
       Name = "test"

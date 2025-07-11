1f0af20a79e135c3f881dddf7371006594e9d84a
vamshikrishnanaliganti <vamshikrishnanaliganti@gmail.com>
2025-07-11 17:56:51 +0530

    mdn

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-  region = "ap-south-1"
+  region = "us-east-1"
 }
 
 resource "aws_instance" "dev" {
-    ami = "ami-0d03cb826412c6b0f"
+    ami = "ami-05ffe3c48a9991133"
     instance_type = "t2.nano"
     # key_name      = ""
 
